31e3e43fa66879314c56c8d97df61facfeedaf24
Samuel Muchoki <njorogemuchoki15@gmail.com>
2024-05-22 10:34:13 +0300

    Best band ever!

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
 -- Ranks country origins of bands, ordered by
 -- the number of (non-unique) fans
-mysql -u root -p < metal_bands.sql;
-SELECT origin, fans AS nb_fans FROM metal_bands ORDER BY nb_fans ASC;
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands
+GROUP BY origin ORDER BY nb_fans DESC;
