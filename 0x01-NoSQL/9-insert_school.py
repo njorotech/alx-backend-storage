cdffc0038da955589af2d230bd611be2d97ecf5b
Samuel Muchoki <njorogemuchoki15@gmail.com>
2024-05-25 22:26:40 +0300

    Insert a document in Python

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,4 +4,4 @@
 
 def insert_school(mongo_collection, **kwargs):
     """inserts a new document in a collection based on kwargs"""
-    mongo_collection.insert_many(kwargs)
+    mongo_collection.insert_one(kwargs)
